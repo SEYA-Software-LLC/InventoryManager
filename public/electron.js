2936d0364ae48d2e76b9bf7dce1add38c7170de6
Noah Garrett <67662284+noahgarrett@users.noreply.github.com>
2022-04-30 09:59:40 -0400

    v0.1.9

--- public/electron.js
+++ public/electron.js
@@ -93,3 +93,10 @@ app.on("web-contents-created", (event, contents) => {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+const sendStatusToWindow = (text) => {
+  log.info(text);
+  if (mainWindow) {
+    mainWindow.webContents.send('message', text);
+  }
+};
--- src/Screens/LoginScreen.js
+++ src/Screens/LoginScreen.js
@@ -1,4 +1,4 @@
-import { Box, Button, Grid, TextField } from '@mui/material'
+import { Box, Button, Grid, TextField, Typography } from '@mui/material'
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios';
@@ -29,6 +29,7 @@ function LoginScreen() {
 
   return (
     <Box>
+      <Typography>v0.1.7</Typography>
       <Grid container spacing={2}>
         <Grid item xs={12}>
           <TextField label="Username" variant="outlined" value={username} onChange={(e) => setUsername(e.target.value)}/>
